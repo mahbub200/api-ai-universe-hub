4b17f14948599f50a3289f44dda9268e0274cb38
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2023-09-06 02:17:08 +0600

    display card info with grid

--- api.js
+++ api.js
@@ -4,28 +4,29 @@ function loadData() {
     .then((data) => displayData(data.data.tools));
 }
 function displayData(data) {
+  // data=data.splice(0,6)
   // console.log(data);
   for (const technology of data) {
     console.log(technology);
     let cardContainer = document.getElementById("card-container");
     let div = document.createElement("div");
-    div.classList.add("card");
+    div.classList.add("col");
     div.innerHTML = `
-        <img src="${technology.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">features:
+    <div class="card">
+    <img src="${technology.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">features:
           
-          </h5>
-          <li>${technology.features[0]}</li>
-          <li>${technology.features[1]}</li>
-          <li>${technology.features[2]}?</li>
-          
-          
-        </div>
-        <div class="card-footer">
+    </h5>
+    <li>${technology.features[0]}</li>
+    <li>${technology.features[1]}</li>
+    <li>${technology.features[2]}</li>
+    </div>
+    <div class="card-footer">
         <h2>${technology.name}</h2> 
         <h2>${technology.published_in}</h2> 
         </div>
+  </div>
         `;
     cardContainer.appendChild(div);
   }
