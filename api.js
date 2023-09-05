2942f5a9e3ce759d4786af7c65168e3dadec49c4
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2023-09-06 02:00:49 +0600

    show all card information

---
+++ api.js
@@ -0,0 +1,34 @@
+function loadData() {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => displayData(data.data.tools));
+}
+function displayData(data) {
+  // console.log(data);
+  for (const technology of data) {
+    console.log(technology);
+    let cardContainer = document.getElementById("card-container");
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <img src="${technology.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">features:
+          
+          </h5>
+          <li>${technology.features[0]}</li>
+          <li>${technology.features[1]}</li>
+          <li>${technology.features[2]}?</li>
+          
+          
+        </div>
+        <div class="card-footer">
+        <h2>${technology.name}</h2> 
+        <h2>${technology.published_in}</h2> 
+        </div>
+        `;
+    cardContainer.appendChild(div);
+  }
+}
+
+loadData();
