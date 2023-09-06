80bfa2b8fa5475b0d5f77b3efd3798230279ed46
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2023-09-07 03:34:37 +0600

    spinner added

--- api.js
+++ api.js
@@ -1,16 +1,26 @@
-function loadData() {
+function loadData(limit) {
+  // load data
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => displayData(data.data.tools));
+    .then((data) => displayData(data.data.tools,limit));
 }
-function displayData(data) {
-  // data=data.splice(0,6)
-  // console.log(data);
+function displayData(data,limit) {
+  let SeeMoreBtn = document.getElementById("see-more");
+if(limit && data.length>6){
+
+  // data = data.splice(0, 6);
+  SeeMoreBtn.classList.remove('d-none')
+}else{
+  SeeMoreBtn.classList.add('d-none')
+}
+  console.log(data);
   for (const technology of data) {
     console.log(technology);
     let cardContainer = document.getElementById("card-container");
     let div = document.createElement("div");
     div.classList.add("col");
+
+    // card info
     div.innerHTML = `
     <div class="card">
     <img src="${technology.image}" class="card-img-top" alt="...">
@@ -28,8 +38,32 @@ function displayData(data) {
         </div>
   </div>
         `;
+    // append child
     cardContainer.appendChild(div);
   }
+  spinnerToggle(false)
 }
+// see more button functionalities
+function searchProcess(limit){
+  loadData(limit)
+}
+function seeMore() {
+  spinnerToggle(true)
+  let SeeMoreBtn = document.getElementById("see-more");
+searchProcess()
+  SeeMoreBtn.classList.add("d-none");
+}
+searchProcess(6)
+// console.log("ddd");
+// spinner toggle handled 
+function spinnerToggle(isLoading){
+  const spinner=document.getElementById('spinner');
+  if(isLoading){
+    spinner.classList.remove('d-none')
+  }
+  else{
+    spinner.classList.add('d-none')
 
-loadData();
+  }
+
+}
